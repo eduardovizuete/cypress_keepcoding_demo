6722befe57b903a9b32b28f6125ad8894765c563
edu <gilberto.vizuete@modinter.com.ec>
2020-07-31 15:16:17 -0500

    test beerflix: login mocking stubs server

--- cypress/integration/beerflix.spec.js
+++ cypress/integration/beerflix.spec.js
@@ -12,12 +12,34 @@ describe('beerflix app', () => {
             .should('include', 'emoji-beer');
     });
 
-    it.only('login', () => {
-       cy.get('@loginData')
+    it('login', () => {
+        cy.server();
+        cy.route('POST', '/api/v1/user/login').as('loginRequest');
+        cy.get('@loginData')
             .then(({ user, fakePwd }) => {
                 cy.log(user)
-                cy.get('[data-cy=email-input]').type('user');
-                cy.get('[data-cy=password-input]').type('fakePwd', {log: false});
+                cy.get('[data-cy=email-input]').type(user);
+                cy.get('[data-cy=password-input]').type(fakePwd, {log: false});
+                cy.wait('@loginRequest');
+                cy.url().should('include', '/home');
+            })
+    });
+
+    it.only('login error', () => {
+        cy.server();
+        cy.route({
+            method: 'POST',
+            url: '/api/v1/user/login',
+            status: 404,
+            response: 'fixture:loginErrorResponse'
+        }).as('loginRequest');
+        cy.get('@loginData')
+            .then(({ user, fakePwd }) => {
+                cy.log(user)
+                cy.get('[data-cy=email-input]').type(user);
+                cy.get('[data-cy=password-input]').type(fakePwd, {log: false});
+                cy.wait('@loginRequest');
+                cy.get('[data-cy=login-error]')
             })
     });
 });
