8918aa235f9d735cf16ab938f1846a8ee73c4958
edu <gilberto.vizuete@modinter.com.ec>
2020-07-31 12:54:38 -0500

    test beerflix: selectors, key events

--- cypress/integration/beerflix.spec.js
+++ cypress/integration/beerflix.spec.js
@@ -1,6 +1,19 @@
 describe('beerflix app', () => {
-    it('visit app', () => {
+    beforeEach(() => {
         cy.visit('/');
+    });
+    
+    
+    it('visit app', () => {
+        cy.get('[data-cy=email-input]');
+        cy.get('[data-cy=login-image]')
+            .should('have.attr', 'src')
+            .should('include', 'emoji-beer');
+    });
+
+    it.only('login', () => {
+        cy.get('[data-cy=email-input]').type('kevin');
+        cy.get('[data-cy=password-input]').type('kevin', {log: false});
     });
 });
 
